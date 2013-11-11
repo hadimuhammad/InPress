68360d02e49e8dc6f5b615bc8968451b0d7426d2
Abdul Hadi Muhammad <hadi.muhammad@gmail.com>
2013-11-11 02:17:30 -0500

    File Structure Modifications

--- Documentation/inPress/module/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from module.models import *
-
-admin.site.register(Assessment)
-admin.site.register(AssessmentData)
-admin.site.register(Courses)
-admin.site.register(MCQuestionData)
