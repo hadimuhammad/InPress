c2782e272770e74a8e337a702e55a8a16faac599
Abdul Hadi Muhammad <hadi.muhammad@gmail.com>
2014-02-05 23:57:43 -0500

    Data Analysis for Completion Done

--- inPress/module/views.py
+++ inPress/module/views.py
@@ -39,6 +39,8 @@ def removeclass(request):
     return render_to_response('removeclass.html', locals()) 
 
 def addclass(request):
+    if (request.method == 'GET'):
+        courses = Courses.objects.all()
     if (request.method == 'POST'):
         try:
             a = Courses.objects.get(CourseName = request.POST['className'])
@@ -87,10 +89,10 @@ def course(request):
     return render_to_response('course.html', locals()) 
 
 def addassessment(request):
-    courses = Courses.objects.all()
     print request
     if (request.method == 'GET'):
         myCourse = request.GET['course']
+        courses = Courses.objects.all()
     if (request.method == 'POST'):
         myCourse = request.POST['course']
         print myCourse
@@ -123,6 +125,7 @@ def removeassessment(request):
 
 def addquestion(request):
     if (request.method == 'GET'):
+        courses = Courses.objects.all()
         myCourse = request.GET['course']
         myAssessment = request.GET['assessment']
         courseName = Courses.objects.filter(CourseName=myCourse)
@@ -143,6 +146,15 @@ def addquestion(request):
             return HttpResponseRedirect('/instructor/course.html?courseInfo='+request.POST['course'])
     return render_to_response('addquestion.html', locals()) 
 
+def data_analysis(request):
+    course = request.GET['course']
+    courses = Courses.objects.all()
+    courseName = Courses.objects.filter(CourseName=course)
+    assessment = request.GET['assessment']
+    assessmentName = Assessment.objects.filter(name = assessment, course=courseName)
+    numOfStudents = Students.objects.filter (CourseName=courseName).count()
+    numOfStudentsComplete = StudentAnswers.objects.filter(Assessment = assessmentName).count()
+    return render_to_response('data_analysis.html', locals()) 
 
 
 def studentcourse(request):
@@ -185,10 +197,19 @@ def viewassessment(request):
         assessments = Assessment.objects.filter(pk =assessmentPK)
         QuestionData = serializers.serialize("json", AssessmentData.objects.filter(Assessment__in=assessments))
         my_param = request.GET.get('isEnd')
-        print my_param
         if (my_param ):
             if (my_param == "true"):
-                return HttpResponseRedirect('/student/course.html?courseInfo='+request.GET['course']+'&studentnumber='+request.GET['studentnumber'])
+                courseName = Courses.objects.filter(CourseName=request.GET['course'])
+                studentno = request.GET['studentnumber']
+                student = Students.objects.get(StudentNumber = studentno, CourseName=courseName)
+                assessment = Assessment.objects.get(pk =assessmentPK)
+                try:
+                    myanswer = StudentAnswers.objects.get (Students = student, Assessment=assessment)
+                    return HttpResponseRedirect('/student/course.html?courseInfo='+request.GET['course']+'&studentnumber='+request.GET['studentnumber'])
+                except StudentAnswers.DoesNotExist:
+                    b = StudentAnswers(Students = student, Assessment=assessment, isDone = True)
+                    b.save()
+                    return HttpResponseRedirect('/student/course.html?courseInfo='+request.GET['course']+'&studentnumber='+request.GET['studentnumber'])
     studentnumber = request.GET['studentnumber']
     mycourses = Students.objects.filter (StudentNumber = request.GET['studentnumber']).values("CourseName")
     courses = Courses.objects.filter (pk__in=mycourses)
@@ -211,7 +232,5 @@ def viewassessmentanswers(request):
         assessmentToDelete = AssessmentData.objects.get(pk=request.POST['QuestionToRemove'])
         assessmentToDelete.delete()
         return HttpResponseRedirect(request.META['HTTP_REFERER'])
-
-
     return render_to_response('viewassessmentanswers.html', locals()) 
 
