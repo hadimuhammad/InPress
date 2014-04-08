4244aa72ff05a1c3429255fe862c9467f148694e
hadimuhammad <hadi.muhammad@gmail.com>
2014-04-08 14:34:48 -0400

    bug in answer

--- inPress/module/views.py
+++ inPress/module/views.py
@@ -257,14 +257,18 @@ def studentcourse(request):
 def studentcoursehistory(request):
     if (request.method == 'POST'):
         studentnumber = request.POST['studentnumber']
-        mycourses = Students.objects.filter (StudentNumber = request.POST['studentnumber']).values("CourseName")
+        studen = Students.objects.filter (StudentNumber = request.POST['studentnumber'])
+	mycourses = Students.objects.filter (StudentNumber = request.POST['studentnumber']).values("CourseName")
         courses = Courses.objects.filter (pk__in=mycourses)
         myCourse = request.POST['course']
         courseName = Courses.objects.filter(CourseName=myCourse)
         assessments = Assessment.objects.filter(course = courseName, post = True, post_date__lte = (timezone.now() - datetime.timedelta(days=1)))
         assessmentData = AssessmentData.objects.filter(Assessment__in=assessments)
-        StudentAnswer = serializers.serialize("json", StudentAnswers.objects.filter(Students__in=studentnumber, AssessmentData__in=assessmentData))
-        ListOfAssessments = serializers.serialize("json", assessments)
+	print studentnumber
+	print StudentAnswers.objects.filter(Students = studen)
+        StudentAnswer = serializers.serialize("json", StudentAnswers.objects.filter(Students = studen))
+        print StudentAnswer
+	ListOfAssessments = serializers.serialize("json", assessments)
         QuestionData = serializers.serialize("json", assessmentData)
     return render_to_response('studentCourseHistory.html', locals())
 
